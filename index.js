e3e93431670fd4ca2763bcd2bbc0fde06bb37691
RaiAn-CSE <68633815+RaiAn-CSE@users.noreply.github.com>
2022-12-24 01:26:43 +0600

    add products

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.3exxtfz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+    try {
+        const products = client.db('rentUsBd').collection('productCollection');
+
+        // get data from server:
+        app.get('/productCollection', async (req, res) => {
+            const query = {}
+            const result = await products.find(query).toArray();
+            res.send(result);
+        });
+        app.post('/productCollection', async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await products.insertOne(user);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
+app.get('/', async (req, res) => {
+    res.send('Home Rent server is running');
+})
+
+app.listen(port, () => console.log(`Home Rent running on ${port}`))
